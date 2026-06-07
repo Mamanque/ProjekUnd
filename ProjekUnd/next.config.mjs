b5d74544d509c18b1ce38fb312021f830e4dd848
Mamanque <71179985+Mamanque@users.noreply.github.com>
2026-06-07 19:35:53 +0700

    Add files via upload
    next js

---
+++ ProjekUnd/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // images: { unoptimized: true }, // Hapus tanda // di depan images jika Anda memakai tag <Image> Next.js
+};
+
+export default nextConfig;
